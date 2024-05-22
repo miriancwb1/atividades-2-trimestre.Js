67df91b7f7e785d1f7c4fd6397b2df44c4f46876
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-05-22 09:54:29 -0300

    Add files via upload

---
+++ NumeroRandamicos.js
@@ -0,0 +1,7 @@
+NumerosRandomicos 
+
+numero_aleatorio = random.randit(1,10)
+print (fnumeroaleatorio = { numero_aleatorio})
+
+numero_aleatorio = [random.randit (1,10) in Range(10)]
+print ("lista de numeros aleatorios:", numero_aleatorio)
